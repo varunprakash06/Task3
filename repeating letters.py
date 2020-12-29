fed3229c1f824e34f21d421f8d02d2bfc1966c8b
varunprakash06 <75519634+varunprakash06@users.noreply.github.com>
2020-12-29 08:01:01 +0530

    Update repeating letters.py

--- repeating letters.py
+++ repeating letters.py
@@ -1,4 +1,14 @@
-from collections import Counter
-Word = input('Please enter the string')
-word_length = Counter(Word)
-print(word_length)
+#Repeating letters in decending oder using dictonary
+Str_in = str(input())
+sorted_out={}
+str_Dic = {}
+def most_frequent(Str_in):
+    for i in Str_in:
+        if i in str_Dic:
+            str_Dic[i] +=1
+        else:
+            str_Dic[i] = 1
+    sorted_out = dict(sorted(str_Dic.items(), key = lambda item:item[1],reverse = True))
+    for i in sorted_out:
+        print(i+":"+str(sorted_out[i]))
+most_frequent(Str_in)
